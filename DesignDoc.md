ff26d1ac7d817aad56b01be99d52d55f3d7e21db
Jared Ni <milkteadj@gmail.com>
2025-03-15 01:02:25 -0400

    start working on client-server with some src code

--- DesignDoc.md
+++ DesignDoc.md
@@ -1,13 +1,26 @@
 **Plan of Attack**
 Phase 1: LSM-tree core components
+0. client-server arch: 
+- https://code.harvard.edu/uts797/cs165-2024-starter-code/blob/master/src/server.c
+- do it correctly for the queries, 
+- load data in client, then send it to server
+- some script that can simulate using docker
+- or just do the server side first
+- then parser, on client side (or maybe split it among client and server, but server side would be fine to start with)
+- 
+
 1. Integrate the data generator found at https://bitbucket.org/HarvardDASlab/cs265-sysproj/src/master/. 
 
 2. Write the parser for the domain specific language of the DB queries. 
 
 3. Buffer (L0) implementation: 
-    - implement in-memory sorted data structure, like a skip list / balanced tree, to buffer writes (fast writes)
+    - implement in-memory sorted data structure, like a skip list / balanced tree, to buffer writes (fast writes)   
+        - begin with just log, skip list / blanaced tree would come later to optimize
+        - buffer should just support the three functions
     - support *put*, *delete*, and in-memory *get* operations. 
     - serialize buffer to disk during flush (e.g. SSTable)
+    - do leveling first, then tiering or lazy can come later
+    - tiering: has different optimizations 
 
 4. Create disk-level data management:
     - design file format for sorted runs (keys, values, min/max keys). 
@@ -21,5 +34,46 @@ Phase 1: LSM-tree core components
 6. add bloom filter to each level with optimized bits per entry to determine if key is not contained in that level.
 7. add fence pointers in each level to allow page or block access within a run. 
 
+8. Multithreading:
+    - start with writes multithread 
+        - each thread takes a chunk of data, and process them
+        - 2D design space: 
+            - split your data, combine
+            if multiple queries, now you can also choose query. How to split threads among query and data? 2D design space
+            one thread take care one query, or each thread take care of 
+    Two cases:
+    - only 1 query: multiplethread single query
+    - multiple query: 
+
+    - Start with one client with bunch of queries, then go multiple clients with bunch of queries
+
 Phase 2: 
 8. save manifest (in-memory and persisted)
+
+
+Monkey: 
+- optimizing best size for bloom filter, size of memory for each level
+Doev: 
+- best merging policy: after you have best bloom filter, what's the best merging policy
+
+
+1. basic (server-client, log for buffer, merging, multithreading)
+2. 4-5 design choices, experimental results. 8 experiments is good. 
+3. make code private, then give TF read access
+
+Before midway check-in: 
+- client-server, all the basic things
+- 2 experiments
+
+final report will be strict: 
+- make your best effort on the midway check-in
+
+3/14 Meet with Stratos:
+Midway checkin experiments:
+- Look at Monkey how to design experiments. 
+- Test with 10, 100, 10000, 1 mil, etc. are the experiments. Start with default hyperparameters
+- multithreading:
+    - reasonable decision to begin with,
+    - next, maybe try something else like 
+    - 
+
---
+++ src/server.c
@@ -0,0 +1,192 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <string.h>
+#include <queue>
+#include <vector>
+#include <algorithm>
+
+#include "common.h"
+#include "parse.h"
+#include "cs165_api.h"
+#include "message.h"
+#include "utils.h"
+#include "client_context.h"
+
+#define DEFAULT_QUERY_BUFFER_SIZE 1024
+#define WORKER_THREADS_NUM 5
+
+
+/** execute_DbOperator takes as input the DbOperator and executes the query.
+ *      What are the structural attributes of a `query`?
+ *      How will you interpret different queries?
+ *      How will you ensure different queries invoke different execution paths in your code?
+ **/
+char* execute_DbOperator(DbOperator* query) {
+    // there is a small memory leak here (when combined with other parts of your database.)
+    // as practice with something like valgrind and to develop intuition on memory leaks, find and fix the memory leak. 
+    if(!query)
+    {
+        return "165";
+    }
+    if(query && query->type == CREATE){
+        if(query->operator_fields.create_operator.create_type == _DB){
+            if (create_db(query->operator_fields.create_operator.name).code == OK) {
+                return "165";
+            } else {
+                return "Failed";
+            }
+        }
+        else if(query->operator_fields.create_operator.create_type == _TABLE){
+            Status create_status;
+            create_table(query->operator_fields.create_operator.db, 
+                query->operator_fields.create_operator.name, 
+                query->operator_fields.create_operator.col_count, 
+                &create_status);
+            if (create_status.code != OK) {
+                cs165_log(stdout, "adding a table failed.");
+                return "Failed";
+            }
+            return "165";
+        }
+    }
+    free(query);
+    return "165";
+}
+
+/**
+ * handle_client(client_socket)
+ * This is the execution routine after a client has connected.
+ * It will continually listen for messages from the client and execute queries.
+ **/
+void handle_client(int client_socket) {
+    int done = 0;
+    int length = 0;
+
+    log_info("Connected to socket: %d.\n", client_socket);
+
+    // Create two messages, one from which to read and one from which to receive
+    message send_message;
+    message recv_message;
+
+    // create the client context here
+    ClientContext* client_context = NULL;
+
+    // Continually receive messages from client and execute queries.
+    // 1. Parse the command
+    // 2. Handle request if appropriate
+    // 3. Send status of the received message (OK, UNKNOWN_QUERY, etc)
+    // 4. Send response to the request.
+    do {
+        length = recv(client_socket, &recv_message, sizeof(message), 0);
+        if (length < 0) {
+            log_err("Client connection closed!\n");
+            exit(1);
+        } else if (length == 0) {
+            done = 1;
+        }
+
+        if (!done) {
+            char recv_buffer[recv_message.length + 1];
+            length = recv(client_socket, recv_buffer, recv_message.length,0);
+            recv_message.payload = recv_buffer;
+            recv_message.payload[recv_message.length] = '\0';
+
+            // 1. parse query into database operator request
+            DbOperator* query = parse_command(recv_message.payload, &send_message, client_socket, client_context);
+
+            // 2. Handle request
+            char* result = execute_DbOperator(query);
+
+            send_message.length = strlen(result);
+            char send_buffer[send_message.length + 1];
+            strcpy(send_buffer, result);
+            send_message.payload = send_buffer;
+            send_message.status = OK_WAIT_FOR_RESPONSE;
+            
+            // 3. Send status of the received message (OK, UNKNOWN_QUERY, etc)
+            if (send(client_socket, &(send_message), sizeof(message), 0) == -1) {
+                log_err("Failed to send message.");
+                exit(1);
+            }
+
+            // 4. Send response to the request
+            if (send(client_socket, result, send_message.length, 0) == -1) {
+                log_err("Failed to send message.");
+                exit(1);
+            }
+        }
+    } while (!done);
+
+    log_info("Connection closed at socket %d!\n", client_socket);
+    close(client_socket);
+}
+
+// setup_server(): set up server side socket
+int setup_server() {
+    int server_socket;
+    size_t len;
+    struct sockaddr_un local;
+
+    log_info("Attempting to setup server...\n");
+
+    if ((server_socket = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+        log_err("L%d: Failed to create socket.\n", __LINE__);
+        return -1;
+    }
+
+    local.sun_family = AF_UNIX;
+    strncpy(local.sun_path, SOCK_PATH, strlen(SOCK_PATH) + 1);
+    unlink(local.sun_path);
+
+    /*
+    int on = 1;
+    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&on, sizeof(on)) < 0)
+    {
+        log_err("L%d: Failed to set socket as reusable.\n", __LINE__);
+        return -1;
+    }
+    */
+
+    len = strlen(local.sun_path) + sizeof(local.sun_family) + 1;
+    if (bind(server_socket, (struct sockaddr *)&local, len) == -1) {
+        log_err("L%d: Socket failed to bind.\n", __LINE__);
+        return -1;
+    }
+
+    if (listen(server_socket, 5) == -1) {
+        log_err("L%d: Failed to listen on socket.\n", __LINE__);
+        return -1;
+    }
+
+    return server_socket;
+}
+
+// TODO: extend to handle multiple clients (how many?)
+// Think about what is shared between clients and what is not
+int main(void)
+{
+    int server_socket = setup_server();
+    if (server_socket < 0) {
+        exit(1);
+    }
+
+    log_info("Waiting for a connection %d ...\n", server_socket);
+
+    struct sockaddr_un remote;
+    socklen_t t = sizeof(remote);
+    int client_socket = 0;
+
+    if ((client_socket = accept(server_socket, (struct sockaddr *)&remote, &t)) == -1) {
+        log_err("L%d: Failed to accept a new connection.\n", __LINE__);
+        exit(1);
+    }
+
+    handle_client(client_socket);
+
+    return 0;
+}
